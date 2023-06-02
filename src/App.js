bba7934101b1099d6498177cb2a6806ce0f3719e
Molly Novash <mollynovash@Mollys-MacBook-Pro.local>
2023-06-02 11:26:08 -0700

    converting app.js from class to hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Component } from 'react';
+// import { Component } from 'react';
+import { useState } from 'react';
 
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
@@ -6,65 +7,80 @@ import SearchBox from './components/search-box/search-box.component';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  // only this component is aware of the local state
-  // constructor method is available on all class components
-  constructor() {
-    super(); // super calls Component's constructor method
+const App = () => {
+  const [searchField, setSearchField] = useState(''); // useState gives us two values that we can destructure- the value we want to store, and a setter function. on the left is where we name these things
+  console.log({ searchField });
 
-    this.state = { // this.state is always a json object
-      monsters: [],
-      searchField: ''
-    };
-    //console.log('constructor');
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLowerCase();
+    setSearchField(searchFieldString);
   }
 
-  // this is a method from the Component class that gets run when a component mounts
-  // mounting is the first time a component gets rendered to the DOM. it only happens once
-  // the only way a component can get remounted, is if it's first unmounted, ie completely removed from the DOM
-  // at that point, its basically a different component ie a new instance of it
-
-  // the moment a component gets mounted onto the dom is when you want to make an api request,
-  // because you want it to display all of the data right away
-  componentDidMount() {
-    // console.log('component did mount');
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) => this.setState(() => {
-        return {monsters: users}
-      },
-      () => {
-        // console.log(this.state);
-      }
-      ));
-  }
-
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLowerCase();
-    this.setState(() => {
-      return { searchField };
-    });
-  }
-  
-  render(){
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
-
-    const filteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(searchField);
-    })
-
-    return (
-      <div className="App">
-        <SearchBox 
-          onChangeHandler={onSearchChange} 
-          placeholder='search monsters'
-          className='search-box'
-        />
-        <CardList monsters={filteredMonsters}/>
-      </div>
-    );
-  }
+  return (
+    <div className='App'>
+      <h1 className='app-title'>Monsters Rolodex</h1>
+      <SearchBox 
+        onChangeHandler={onSearchChange} 
+        placeholder='search monsters'
+        className='monsters-search-box'
+      />
+    </div>
+  );
 }
+
+// class App extends Component {
+//   // only this component is aware of the local state
+//   // constructor method is available on all class components
+//   constructor() {
+//     super(); // super calls Component's constructor method
+
+//     this.state = { // this.state is always a json object
+//       monsters: [],
+//       searchField: ''
+//     };
+//     //console.log('constructor');
+//   }
+
+//   componentDidMount() {
+//     // console.log('component did mount');
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//       .then((response) => response.json())
+//       .then((users) => this.setState(() => {
+//         return {monsters: users}
+//       },
+//       () => {
+//         // console.log(this.state);
+//       }
+//       ));
+//   }
+
+//   onSearchChange = (event) => {
+//     const searchField = event.target.value.toLowerCase();
+//     this.setState(() => {
+//       return { searchField };
+//     });
+//   }
+  
+//   render(){
+//     const { monsters, searchField } = this.state;
+//     const { onSearchChange } = this;
+
+//     const filteredMonsters = monsters.filter((monster) => {
+//       return monster.name.toLowerCase().includes(searchField);
+//     })
+
+//     return (
+//       <div className="App">
+//         <h1 className='app-title'>Monsters Rolodex</h1>
+//         <SearchBox 
+//           onChangeHandler={onSearchChange} 
+//           placeholder='search monsters'
+//           className='monsters-search-box'
+//         />
+//         <CardList monsters={filteredMonsters}/>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,18 +1,21 @@
 import { Component } from 'react';
+import Card from '../card/card.component';
+import './card-list.styles.css';
 
 class CardList extends Component {
-    
     render() {
         // this is called destructuring, so you don't have to keep typing 'this'
         const { monsters } = this.props;
 
         return (
-            <div>
-                {monsters.map((monster) => (
-                    <h1 key={monster.id}>{monster.name}</h1>
-                ))}
+            <div className='card-list'>
+                {monsters.map((monster) => {
+                    return (
+                        <Card monster={monster}/>
+                    );
+                })}
             </div>
-        );
+        ); 
     }
 }
 
