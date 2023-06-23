b0ea650cb975e14fca65bd4ca8adb15e1cc8e4df
Molly Novash <mollynovash@Mollys-MacBook-Pro.local>
2023-06-22 18:09:30 -0700

    converted whole app to functional components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,22 +1,15 @@
-import { Component } from 'react';
 import Card from '../card/card.component';
 import './card-list.styles.css';
 
-class CardList extends Component {
-    render() {
-        // this is called destructuring, so you don't have to keep typing 'this'
-        const { monsters } = this.props;
-
-        return (
-            <div className='card-list'>
-                {monsters.map((monster) => {
-                    return (
-                        <Card monster={monster}/>
-                    );
-                })}
-            </div>
-        ); 
-    }
-}
+const CardList = ({ monsters }) => (
+  // this is called destructuring, so you don't have to keep typing 'this'
+  <div className='card-list'>
+      {monsters.map((monster) => {
+          return (
+              <Card monster={monster}/>
+          );
+      })}
+  </div>
+); 
 
 export default CardList;
